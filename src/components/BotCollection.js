3b31eb773fffb5409844883321ac2fa22865389e
Lorraineken <lorrainekupa@gmail.com>
2023-01-23 12:02:59 +0300

    Added button for deleting a bot

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,16 @@
 import React,{useEffect} from "react";
 
-function BotCollection({bots}){
+function BotCollection({bots,deleteBot}){
 
     const collection = bots.map((item) => 
-    <div key ={item.id}>
-    <img  src={item.avatar_url} alt={`${item.name} ${item.bot_class}`} />
+    <div key ={item.id} className='card container-md col-6 col-sm-3  bot_display'>
+    <img className="card-img-top" src={item.avatar_url} alt={`${item.name} ${item.bot_class}`} />
     <p>Name: {item.name}</p>
     <p>Health: {item.health}</p>
     <p>Armor: {item.armor}</p>
     <p>BotClass: {item.bot_class}</p>
     <p>Catchphrase: {item.catchphrase}</p>
+    <button className="btn btn-danger" onClick={deleteBot}>X</button>
     </div>
     )
 
@@ -17,7 +18,7 @@ function BotCollection({bots}){
 
     return (
 
-        <div>
+        <div className="row">
          {collection}
         </div>
     )
