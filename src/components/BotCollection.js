6a2826a6f1ce47f3ff1417a644c25117e57dc791
Lorraineken <lorrainekupa@gmail.com>
2023-01-23 11:32:03 +0300

    Updated bots displayed details

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -2,7 +2,16 @@ import React,{useEffect} from "react";
 
 function BotCollection({bots}){
 
-    const collection = bots.map((item) => <img key ={item.id} src={item.avatar_url} alt={`${item.name} ${item.bot_class}`} />)
+    const collection = bots.map((item) => 
+    <div key ={item.id}>
+    <img  src={item.avatar_url} alt={`${item.name} ${item.bot_class}`} />
+    <p>Name: {item.name}</p>
+    <p>Health: {item.health}</p>
+    <p>Armor: {item.armor}</p>
+    <p>BotClass: {item.bot_class}</p>
+    <p>Catchphrase: {item.catchphrase}</p>
+    </div>
+    )
 
 
 
