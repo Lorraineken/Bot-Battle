9925454d1b0a8752880a569777616791ede39ec8
Lorraineken <lorrainekupa@gmail.com>
2023-01-23 11:22:27 +0300

    Fetched bots

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 import BotCollection from './BotCollection';
-
+import YourBotArmy from './YourBotArmy';
 
 function App() {
+
+  const[bots,setBots] = useState([])
+   
+  useEffect(() =>{
+    fetch("http://localhost:3000/bots")
+   .then(response => response.json())
+   .then((botsData) => setBots(botsData)
+   )
+
+  },[]) 
+
+  console.log(bots)
+
   return (
     <div>
-     
+     <BotCollection bots ={bots}/>
     </div>
   );
 }
--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React,{useEffect} from "react";
 
-function BotCollection(){
+function BotCollection({bots}){
+
+    const collection = bots.map((item) => <img key ={item.id} src={item.avatar_url} alt={`${item.name} ${item.bot_class}`} />)
+
 
 
     return (
 
         <div>
-
+         {collection}
         </div>
     )
 }
